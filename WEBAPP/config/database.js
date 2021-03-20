13a3cc343ae2c8b6003df6d2dce85df12e0e2db1
carol80 <carolseb80@gmail.com>
2021-03-20 14:18:42 +0530

    webapp

---
+++ WEBAPP/config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+  host: "stocks-main-db.csztviryeyjm.ap-south-1.rds.amazonaws.com",
+  user: "postgres",
+  database: "postgres",
+  // password: "Prince@99",
+  password: "123456",
+  port: "5432",
+}
