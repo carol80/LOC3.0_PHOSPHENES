a233173122833ce539e80915bbc796f4277bbda9
Rach <racheljose11@yahoo.com>
2021-03-20 17:47:00 +0530

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,57 +1,82 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import Header from './Header'
-const Formjs = (props) => {
-  return (
-    <div>
-        <Header/>
-    <Form>
-      <FormGroup>
-        <Label  for="topic">Topic</Label>
-        <Input type="text" name="topic" id="topic" placeholder="topic" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="Location">Location</Label>
-        <Input type="text" name="location"  placeholder="location" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="Destination">Destination</Label>
-        <Input type="text" name="Destination"  placeholder="Destination" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="Date">Date</Label>
-        <Input
-          type="date"
-          name="date"
-          placeholder="date"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="startTime">Start Time</Label>
-        <Input
-          type="time"
-          name="stime"
-          placeholder="start time"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="endTime">Start Time</Label>
-        <Input
-          type="time"
-          name="etime"
-          placeholder="end time"
-        />
-      </FormGroup>
-      
-      
-      
-      
-      
-      <Button>Submit</Button>
-    </Form>
-    </div>
-    
-  );
+class Formjs extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        topic:null,
+        location:null,
+        destination:null,
+        date:null,
+        stime:null,
+        etime:null
+    }
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+        
+    }
+    render(){
+        return (
+            <div>
+                <Header/>
+            <Form>
+              <FormGroup>
+                <Label  for="topic">Topic</Label>
+                <Input type="text" name="topic" id="topic" placeholder="topic" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="Location">Location</Label>
+                <Input type="text" name="location"  placeholder="location" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="Destination">Destination</Label>
+                <Input type="text" name="destination"  placeholder="Destination" />
+              </FormGroup>
+              <FormGroup>
+                <Label for="Date">Date</Label>
+                <Input
+                  type="date"
+                  name="date"
+                  placeholder="date"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="startTime">Start Time</Label>
+                <Input
+                  type="time"
+                  name="stime"
+                  placeholder="start time"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="endTime">Start Time</Label>
+                <Input
+                  type="time"
+                  name="etime"
+                  placeholder="end time"
+                />
+              </FormGroup>
+              
+              
+              
+              
+              
+              <Button>Submit</Button>
+            </Form>
+            </div>
+            
+          );
+    }
+  
 }
 
 export default Formjs;
